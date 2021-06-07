2edf6e2eda82479b6108cb0c8063c792fcb64a32
LucaQuacquarelli <lucaq82@libero.it>
2021-06-07 17:00:27 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Lato', sans-serif;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,122 @@
+<template>
+  <header>
+      <div class="logo-container">
+          <a href="#">
+              <img src="../assets/img/dc-logo.png" alt="dc logo">
+          </a>
+      </div>
+      <nav>
+          <ul>
+              <li v-for="(link, index) in links" 
+              :key="index" 
+              :class="index == activeIndex ? 'active': ''"
+              @click="setActive(index)">
+                <a :href="link.url">{{link.name}}</a>
+              </li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            links: [
+                {
+                    name: "Characters",
+                    url: "#characters"
+                },
+                {
+                    name: "Comics",
+                    url: "#comics"
+                },
+                {
+                    name: "Movies",
+                    url: "#movies"
+                },
+                {
+                    name: "Tv",
+                    url: "#tv"
+                },
+                {
+                    name: "Games",
+                    url: "#games"
+                },
+                {
+                    name: "Collectibles",
+                    url: "#collectibles"
+                },
+                {
+                    name: "Videos",
+                    url: "#videos"
+                },
+                {
+                    name: "Fans",
+                    url: "#fans"
+                },
+                {
+                    name: "News",
+                    url: "#news"
+                },
+                {
+                    name: "Shop",
+                    url: "#shop"
+                },
+            ],
+            activeIndex: 1
+        }
+    },
+    methods: {
+        setActive: function(newIndex) {
+            this.activeIndex = newIndex;
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px;
+
+        img {
+            width: 70%;
+        }
+        
+        nav {
+
+            ul {
+                display: flex;
+
+                li {
+                    margin: 0 10px;
+                    padding: 30px 0;
+                    list-style: none;
+
+                    a {
+                        text-decoration: none;
+                        text-transform: uppercase;
+                        color: black;
+                    }
+
+                    &:hover,
+                    &.active  {
+                        border-bottom: 3px solid #0282f9;
+                    }
+
+                    &:hover a,
+                    &.active a {
+                        color: #0282f9;
+                    }
+
+                }
+
+            }
+            
+        }
+
+    }
+</style>
